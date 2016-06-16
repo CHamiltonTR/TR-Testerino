05c1426d4f5d2bc7d5e13c3a0afe05f4f29cd1c9
CHamiltonTR <chris.hamilton@thomsonreuters.com>
2016-06-16 10:53:18 -0400

    Add files via upload

---
+++ angularDropdown.js
@@ -0,0 +1,27 @@
+/**
+ * Created by U6040257 on 6/15/2016.
+ */
+
+angular.module('ui.bootstrap.demo').controller('DropdownCtrl', function ($scope, $log) {
+    $scope.items = [
+        'The first choice!',
+        'And another choice for you.',
+        'but wait! A third!'
+    ];
+
+    $scope.status = {
+        isopen: false
+    };
+
+    $scope.toggled = function(open) {
+        $log.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+
+    $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+});
